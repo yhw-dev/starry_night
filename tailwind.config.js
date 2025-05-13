92f7190bf03d4697cc4e8466441700fd0d0d3d8c
yhw-dev <yhwdev@gmail.com>
2025-05-13 11:15:30 +0900

    Tailwindcss 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
