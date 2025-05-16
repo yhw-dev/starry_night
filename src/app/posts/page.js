3db6b91d9acf3f4f0bd45ff80135932d5724f48c
yhw-dev <yhwdev@gmail.com>
2025-05-16 20:47:52 +0900

    표기 수정

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -65,7 +65,7 @@ export default function PostsPage() {
 
   return (
     <div className="flex flex-col items-center py-10">
-      <h1 className="text-2xl font-bold mb-4 text-white">게시글 목록</h1>
+      <h1 className="text-2xl font-bold mb-4 text-white">별무리</h1>
 
       <div className="w-full max-w-md mb-6 px-4">
         <SearchBar onSearch={handleSearch} />
